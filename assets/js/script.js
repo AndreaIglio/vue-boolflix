babfd3082ab847477fc0a5386e7905e57b6889ad
Andrea Iglio <andrea.iglio@gmail.com>
2021-01-12 13:14:07 +0100

    Milestone 2 (Added Stars)
    Added Stars below average_vote based on its average_vote

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,17 @@ let app = new Vue({
             ).then(response=>{
                 console.log(response.data.results);
                 this.movieList = response.data.results;
+               
+                this.movieList.forEach(element => {
+                    // console.log(element.vote_average);
+                    let vote = Math.round(element.vote_average/2);
+                    // console.log(vote);
+                    element.vote_average = vote;
+                    element.max_vote = 5;
+                   
+                });
+
+
             })
             ;
 
