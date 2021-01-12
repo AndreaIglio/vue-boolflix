c5a25ca60ce7d262422b1be242f016efad6f0474
Andrea Iglio <andrea.iglio@gmail.com>
2021-01-12 11:48:05 +0100

    Milestone 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,30 @@ let app = new Vue({
     data:{
 
         initValue: '',
+        movieList:[],
+
+    },
+    methods: {
+
+        searchMovie : function (){
+
+            axios.get(
+              `https://api.themoviedb.org/3/search/movie?api_key=ddf88c3ce2b6d4e123fdc23f9bae3d52&language=en-US&&query=${this.initValue}&page=1&include_adult=false`
+            ).then(response=>{
+                console.log(response.data.results);
+                this.movieList = response.data.results;
+            })
+            ;
+
+        }
 
 
     },
-    methods: {},
     mounted() {
         
+
+
     },
 })
+
+// titolo titolo originale lingua voto
