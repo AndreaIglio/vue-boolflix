fa91a0e4454b36252947e7b713c63dfd8ff42670
Andrea Iglio <andrea.iglio@gmail.com>
2021-01-12 18:13:21 +0100

    Milestone 2 added icons languages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,40 @@
 let app = new Vue({
-    el:"#root",
-    data:{
+  el: "#root",
+  data: {
+    initValue: "",
+    movieList: [],
+  },
+  methods: {
+    searchMovie: function () {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/search/movie?api_key=ddf88c3ce2b6d4e123fdc23f9bae3d52&language=en-US&&query=${this.initValue}&page=1&include_adult=false`
+        )
+        .then((response) => {
 
-        initValue: '',
-        movieList:[],
+            // Milestone 1,2
 
+            // I bind to our movieList the response from the database.
+
+          this.movieList = response.data.results;
+
+            // To each element of our movieList i create a variable vote which i bind the value with math.ceil method   
+          this.movieList.forEach((element) => {
+            let vote = Math.ceil(element.vote_average / 2);
+            element.vote_average = vote;
+            // Creating a property to every of our object to be able to have a maximum of stars
+            element.max_vote = 5;
+
+            
+          });
+        });
     },
-    methods: {
 
-        searchMovie : function (){
-
-            axios.get(
-              `https://api.themoviedb.org/3/search/movie?api_key=ddf88c3ce2b6d4e123fdc23f9bae3d52&language=en-US&&query=${this.initValue}&page=1&include_adult=false`
-            ).then(response=>{
-                console.log(response.data.results);
-                this.movieList = response.data.results;
-               
-                this.movieList.forEach(element => {
-                    // console.log(element.vote_average);
-                    let vote = Math.round(element.vote_average/2);
-                    // console.log(vote);
-                    element.vote_average = vote;
-                    element.max_vote = 5;
-                   
-                });
+    
+  },
+  mounted() {},
+});
 
 
-            })
-            ;
 
-        }
-
-
-    },
-    mounted() {
-        
-
-
-    },
-})
-
-// titolo titolo originale lingua voto
+// Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera dellanazione corrispondente, gestendo il caso in cui non abbiamo la bandiera dellanazione ritornata dall’API (le flag non ci sono in FontAwesome).Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricercadovremo prendere sia i film che corrispondono alla query, sia le serie tv, standoattenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON dirisposta diversi, simili ma non sempre identici)Qui un esempio di chiamata per le serie tv:https://api.themoviedb.org/3/search/tv?
