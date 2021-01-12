85c05dc4f9b65df0b28f57c773d65833859832af
Andrea Iglio <andrea.iglio@gmail.com>
2021-01-12 20:54:23 +0100

    Milestone 2 bug fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,10 @@ let app = new Vue({
   },
   methods: {
     searchMovie: function () {
-      axios
-        .get(
+      axios.get(
           `https://api.themoviedb.org/3/search/movie?api_key=ddf88c3ce2b6d4e123fdc23f9bae3d52&language=en-US&&query=${this.initValue}&page=1&include_adult=false`
         )
-        .then((response) => {
+        .then(response => {
 
             // Milestone 1,2 (add stars to show the vote_average)
 
@@ -25,8 +24,6 @@ let app = new Vue({
             element.vote_average = vote;
             // Creating a property to every of our object to be able to have a maximum of stars
             element.max_vote = 5;
-
-            
           });
         //   console.log(this.searchList);
         });
@@ -34,9 +31,9 @@ let app = new Vue({
 
         // Milestone 2 (add tv shows to the list)
 
-        axios.get(
+      axios.get(
           `https://api.themoviedb.org/3/search/tv?api_key=ddf88c3ce2b6d4e123fdc23f9bae3d52&language=it_IT&query=${this.initValue}`
-        ).then(response=>{
+      ).then(response=>{
         //   console.log(response.data.results);
           let tvShows = response.data.results;
             console.log(tvShows);
@@ -54,18 +51,11 @@ let app = new Vue({
               max_vote: 5,
               
             });
-          })
+          });
 
         //   console.log(this.searchList);
 
         });
     },
-
-    
   },
-  mounted() {},
 });
-
-
-
-// Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera dellanazione corrispondente, gestendo il caso in cui non abbiamo la bandiera dellanazione ritornata dall’API (le flag non ci sono in FontAwesome).Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricercadovremo prendere sia i film che corrispondono alla query, sia le serie tv, standoattenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON dirisposta diversi, simili ma non sempre identici)Qui un esempio di chiamata per le serie tv:https://api.themoviedb.org/3/search/tv?
