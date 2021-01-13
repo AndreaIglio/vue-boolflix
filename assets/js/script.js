47b1ae261ad950e17071848c52ac10777d240425
Andrea Iglio <andrea.iglio@gmail.com>
2021-01-13 10:16:58 +0100

    Milestone 3 completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,58 +3,72 @@ let app = new Vue({
   data: {
     initValue: "",
     searchList: [],
+    imageUrl: "https://image.tmdb.org/t/p/w185",
   },
   methods: {
     searchMovie: function () {
-      axios.get(
+      axios
+        .get(
           `https://api.themoviedb.org/3/search/movie?api_key=ddf88c3ce2b6d4e123fdc23f9bae3d52&language=en-US&&query=${this.initValue}&page=1&include_adult=false`
         )
-        .then(response => {
+        .then((response) => {
+          // Milestone 1,2 (add stars to show the vote_average)
 
-            // Milestone 1,2 (add stars to show the vote_average)
-
-            // I bind to our searchList the response from the database.
+          // I bind to our searchList the response from the database.
 
           this.searchList = response.data.results;
 
-            // To each element of our searchList i create a variable vote which i bind the value with math.ceil method   
+          // To each element of our searchList i create a variable vote which i bind the value with math.ceil method
           this.searchList.forEach((element) => {
-            element.type = 'Movie';
+            element.type = "Movie";
             let vote = Math.ceil(element.vote_average / 2);
             element.vote_average = vote;
             // Creating a property to every of our object to be able to have a maximum of stars
             element.max_vote = 5;
+            let coverImg = element.poster_path;
+            element.poster_path = `${this.imageUrl}${coverImg}`
+            // element.poster_path = `${this.imageUrl} element.poster_path`;
           });
-        //   console.log(this.searchList);
+          //   console.log(this.searchList);
         });
 
+      // Milestone 2 (add tv shows to the list)
 
-        // Milestone 2 (add tv shows to the list)
-
-      axios.get(
+      axios
+        .get(
           `https://api.themoviedb.org/3/search/tv?api_key=ddf88c3ce2b6d4e123fdc23f9bae3d52&language=it_IT&query=${this.initValue}`
-      ).then(response=>{
-        //   console.log(response.data.results);
+        )
+        .then((response) => {
+          //   console.log(response.data.results);
           let tvShows = response.data.results;
-            console.log(tvShows);
-          tvShows.forEach(element=>{
+          console.log(tvShows);
+          tvShows.forEach((element) => {
+
+ 
+            // Milestone 3
+            // i create a variable to assign the value of poster_path property, then i assign to the element.poster_path the value of the standardize imageUrl + the element.poster_path value
+
+             let coverImg = element.poster_path;
+             element.poster_path = `${this.imageUrl}${coverImg}`;
+
             // console.log(element);
+            //Push new objects into the array, not to get the error i need already to push them with the standard proprierties of searchList[], otherway it reminds as that the array length has some error
 
-            //Push new objects into the array, not to get the error i need allready to push them with the standard proprierties of searchList
+           
 
+            console.log(element);
             this.searchList.push({
               title: element.name,
               original_title: element.original_name,
               original_language: element.original_language,
-              type: 'Tv show',
+              type: "Tv show",
+              poster_path: element.poster_path,
               vote_average: Math.ceil(element.vote_average / 2),
               max_vote: 5,
-              
             });
           });
 
-        //   console.log(this.searchList);
-
+          //   console.log(this.searchList);
         });
     },
   },
