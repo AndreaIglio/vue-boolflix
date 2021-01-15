dd9c040c45e582645470011e834b905ceddce403
Andrea Iglio <andrea.iglio@gmail.com>
2021-01-15 15:48:49 +0100

    Milestone 5 Task1
    Added actors to the movies/tvshow description card, then i added even overview of tvshow and some more css style to the cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,16 @@ let app = new Vue({
   data: {
     initValue: "",
     searchList: [],
+
     imageUrl: "https://image.tmdb.org/t/p/w342",
     castListName: [],
   },
   methods: {
-    searchMovie: function () {
+      
+    searchMovie: function() {
+
+
+    
       axios
         .get(
           `https://api.themoviedb.org/3/search/movie?api_key=ddf88c3ce2b6d4e123fdc23f9bae3d52&language=en-US&&query=${this.initValue}&page=1&include_adult=false`
@@ -34,7 +39,7 @@ let app = new Vue({
             let coverImg = element.poster_path;
             element.poster_path = `${this.imageUrl}${coverImg}`;
 
-            let actorName = [];
+            let cast = {};
             // Milestone 5
             
             axios
@@ -42,49 +47,27 @@ let app = new Vue({
                 `https://api.themoviedb.org/3/movie/${element.id}/credits?api_key=ddf88c3ce2b6d4e123fdc23f9bae3d52&language=en-US`
               )
               .then((response) => {
-                // OPZIONE 1
+
                 // console.log(response.data.cast);
-                // this.castListName = response.data.cast.slice(0, 5);
-                // // console.log(this.castListName);
+                let actorMovieName = response.data.cast.slice(0, 5);
 
-                // for (let i = 0; i < this.castListName.length; i++) {
-                //   console.log(this.castListName[i].name);
-                //   this.castListName[i];
-                //   element["actor" + i] = this.castListName[i].name;
-                // }
 
-                // OPZIONE 2
-                for (let i = 0; i < 5; i++) {
-                  // console.log(response.data.cast[i].name);
-                  
-                  actorName.push(response.data.cast[i].name);
+                for (let i = 0; i < actorMovieName.length; i++) {
+                  // console.log(actorMovieName[i].name);
+                  Vue.set(cast, i, actorMovieName[i].name)
                 }
 
-                console.log(actorName);
-
-
-                //OPZIONE 3
-                //   let actorName = [];
-
-                //   for (let i = 0; i < this.castListName.length; i++) {
-                //     console.log(this.castListName[i].name);
-                //     this.castListName[i];
-                //     // element["actor" + i] = this.castListName[i].name;
-                //     actorName.push(this.castListName[i].name);
-                //     // console.log(actorName);
-
-                //     this.actorName = actorName;
-                //   }
-                //   console.log(actorName);
-                //   console.log(this.actorName);
+              //  console.log(cast);
+               Vue.set(element, 'actors', cast);
+                      
               });
-
-              console.log(actorName);
               
-              element.actor = actorName;
-              console.log(element.actor);
+           
+              // console.log(this.searchList);
+              // let searchListParsed = JSON.parse(JSON.stringify(this.searchList));
+              // console.log(searchListParsed);
 
-            //   console.log(this.searchList);
+
           });
         });
 
@@ -110,27 +93,53 @@ let app = new Vue({
             let coverImg = element.poster_path;
             element.poster_path = `${this.imageUrl}${coverImg}`;
 
+            console.log(element);
+
+            let tvShowCast = {};
+
+            axios.get(
+              `https://api.themoviedb.org/3/tv/${element.id}/credits?api_key=ddf88c3ce2b6d4e123fdc23f9bae3d52&language=en-US`
+            )
+            .then(response => {
+              // console.log(response.data.cast);
+
+              let actorTvShowName = response.data.cast.slice(0,5);
+              // console.log(actorTvShowName);
+
+              for(let i=0 ; i< actorTvShowName.length; i++){
+                // console.log(actorTvShowName[i].name);
+                Vue.set(tvShowCast, i, actorTvShowName[i].name);
+              }
+
+              console.log(tvShowCast);
+              
+
+            });
+            
             
             // console.log(element);
             //Push new objects into the array, not to get the error i need already to push them with the standard proprierties of searchList[], otherway it reminds as that the array length has some error
 
-            console.log(element);
+            // console.log(element);
             this.searchList.push({
               title: element.name,
               original_title: element.original_name,
               original_language: element.original_language,
               type: "Tv show",
+              overview: element.overview,
               poster_path: element.poster_path,
               vote_average: Math.ceil(element.vote_average / 2),
               max_vote: 5,
+              actors: tvShowCast,
             });
           });
 
           //   console.log(this.searchList);
         });
+
+    // console.log(this.searchList);
+
     },
   },
-  beforeUpdate() {
-    actorName = [];
-  },
+  
 });
